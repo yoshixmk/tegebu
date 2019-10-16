dcbb89b21b7c6bf10bb42bdfbf67b66a6532cd2c
jamashita <jamashita@arachnid.xyz>
2019-10-17 01:08:49 +0900

    revmoe src/images dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,13 +10,6 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      }
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
         name: 'config',
         path: `${__dirname}/src/config`,
       }
