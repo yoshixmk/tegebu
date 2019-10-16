7f3f991f9983a8cfa06f53285f9abaa76868a2ef
jamashita <jamashita@arachnid.xyz>
2019-10-16 22:04:40 +0900

    metas, links are in the Helmet tag

--- src/layout/Application.tsx
+++ src/layout/Application.tsx
@@ -15,41 +15,107 @@ export class Application extends React.Component<Props, State> {
   public render(): React.ReactElement {
     return (
       <div className='application'>
-        <Helmet>
-          <title>Tegebu - Tabletop Games Club</title>
-
-          <meta charSet='utf-8'/>
-          <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'/>
-
-          <link href='https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900' rel='stylesheet'/>
-
-          <link rel='stylesheet' href='css/open-iconic-bootstrap.min.css'/>
-          <link rel='stylesheet' href='css/animate.css'/>
-
-          <link rel='stylesheet' href='css/owl.carousel.min.css'/>
-          <link rel='stylesheet' href='css/owl.theme.default.min.css'/>
-          <link rel='stylesheet' href='css/magnific-popup.css'/>
-
-          <link rel='stylesheet' href='css/aos.css'/>
-
-          <link rel='stylesheet' href='css/ionicons.min.css'/>
-
-          <link rel='stylesheet' href='css/bootstrap-datepicker.css'/>
-          <link rel='stylesheet' href='css/jquery.timepicker.css'/>
-
-
-          <link rel='stylesheet' href='css/flaticon.css'/>
-          <link rel='stylesheet' href='css/icomoon.css'/>
-          <link rel='stylesheet' href='css/style.css'/>
-
-          <link rel='apple-touch-icon' sizes='180x180' href='./favicons/apple-touch-icon.png'/>
-          <link rel='icon' type='image/png' sizes='32x32' href='./favicons/favicon-32x32.png'/>
-          <link rel='icon' type='image/png' sizes='16x16' href='./favicons/favicon-16x16.png'/>
-          <link rel='manifest' href='./favicons/site.webmanifest'/>
-          <link rel='mask-icon' href='./favicons/safari-pinned-tab.svg' color='#5bbad5'/>
-          <meta name='msapplication-TileColor' content='#da532c'/>
-          <meta name='theme-color' content='#ffffff'/>
-        </Helmet>
+        <Helmet
+          title='Tegebu - Tabletop Games Club'
+          meta={[
+            {
+              name: 'char-set',
+              content: 'utf-8'
+            },
+            {
+              name: 'viewport',
+              content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+            },
+            {
+              name: 'msapplication-TileColor',
+              content: '#da532c'
+            },
+            {
+              name: 'theme-color',
+              content: '#ffffff'
+            }
+          ]}
+          link={[
+            {
+              href: 'https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/open-iconic-bootstrap.min.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/animate.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/owl.carousel.min.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/owl.theme.default.min.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/magnific-popup.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/aos.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/ionicons.min.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/bootstrap-datepicker.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/jquery.timepicker.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/flaticon.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/icomoon.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: 'css/style.css',
+              rel: 'stylesheet'
+            },
+            {
+              href: './favicons/apple-touch-icon.png',
+              rel: 'apple-touch-icon',
+              sizes: '180x180'
+            },
+            {
+              href: './favicons/favicon-32x32.png',
+              rel: 'icon',
+              sizes: '32x32',
+              type: 'image/png'
+            },
+            {
+              href: './favicons/favicon-16x16.png',
+              rel: 'icon',
+              sizes: '16x16',
+              type: 'image/png'
+            },
+            {
+              href: './favicons/site.webmanifest',
+              rel: 'manifest'
+            },
+            {
+              href: './favicons/safari-pinned-tab.svg',
+              rel: 'mask-icon',
+              color: '#5bbad5'
+            }
+          ]}
+        />
       </div>
     );
   }
