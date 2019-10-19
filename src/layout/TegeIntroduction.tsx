9cb85389194ada16f98946a00b97cdbbe6d6fbbc
jamashita <jamashita@arachnid.xyz>
2019-10-19 23:11:18 +0900

    table-top games imported

--- src/layout/TegeIntroduction.tsx
+++ src/layout/TegeIntroduction.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Tege} from '../type/Tege';
 
-type Props = {
+type Props = Tege & {
+  index: number;
 };
 type State = {
 };
@@ -8,30 +10,71 @@ type State = {
 export class TegeIntroduction extends React.Component<Props, State> {
 
   public render(): React.ReactElement {
+    const {
+      tegename,
+      tegegenre,
+      tegeeasiness,
+      tegetime,
+      tegepersons,
+      tegeage,
+      tegedescription,
+      tegeimpression,
+      tegeimage,
+      index
+    } = this.props;
+
+    if (index % 2 === 0) {
+      return (
+        <div className='coach d-sm-flex align-items-stretch'>
+          <div className='text py-4 px-5 ftco-animate col-lg-8'>
+            <h5><a href='#'>1. てげ名</a></h5>
+            <p>{tegename}</p>
+            <h5><a href='#'>2. てげ分別</a></h5>
+            <p>{tegegenre}</p>
+            <h5><a href='#'>3. てげ軽さ</a></h5>
+            <p>{tegeeasiness}</p>
+            <h5><a href='#'>4. てげ時間</a></h5>
+            <p>{tegetime} 分</p>
+            <h5><a href='#'>5. てげ人数</a></h5>
+            <p>{tegepersons}</p>
+            <h5><a href='#'>6. てげ適齢期</a></h5>
+            <p>{tegeage} 歳</p>
+            <h5><a href='#'>7. てげ概要</a></h5>
+            <p>{tegedescription}</p>
+            <h5><a href='#'>8. てげ感想</a></h5>
+            <p>{tegeimpression}</p>
+          </div>
+          <div className='img col-lg-4' style={{
+            backgroundImage: `url(images/teges/${tegeimage})`
+          }}/>
+        </div>
+      );
+    }
+
     return (
-      <section className='ftco-about d-md-flex'>
-        <div className='one-half img' style={{
-          backgroundImage: 'url(images/about.jpg)'
-        }}>
-          <a href='https://vimeo.com/45830194' className='icon popup-vimeo d-flex justify-content-center align-items-center'>
-            <span className='icon-play'/>
-          </a>
+      <div className='coach d-sm-flex align-items-stretch'>
+        <div className='img col-lg-4' style={{
+          backgroundImage: `url(images/teges/${tegeimage})`
+        }}/>
+        <div className='text py-4 px-5 ftco-animate col-lg-8'>
+          <h5><a href='#'>1. てげ名</a></h5>
+          <p>${tegename}</p>
+          <h5><a href='#'>2. てげ分別</a></h5>
+          <p>${tegegenre}</p>
+          <h5><a href='#'>3. てげ軽さ</a></h5>
+          <p>${tegeeasiness}</p>
+          <h5><a href='#'>4. てげ時間</a></h5>
+          <p>${tegetime} 分</p>
+          <h5><a href='#'>5. てげ人数</a></h5>
+          <p>${tegepersons}</p>
+          <h5><a href='#'>6. てげ適齢期</a></h5>
+          <p>${tegeage} 歳</p>
+          <h5><a href='#'>7. てげ概要</a></h5>
+          <p>${tegedescription}</p>
+          <h5><a href='#'>8. てげ感想</a></h5>
+          <p>${tegeimpression}</p>
         </div>
-        <div className='one-half ftco-animate'>
-          <div className='heading-section ftco-animate '>
-            <h2 className='mb-4'>Tegebu Since 2019</h2>
-          </div>
-          <div>
-            <p>Tegebu is an abbreviation of Japanese-like pronunciation of 'tabletop games club' as 'teburutoppu gemusu kurabu'. In other languages, Tegebu is called in such ways.</p>
-            <p>Tabletop games club (English: English)</p>
-            <p>卓上遊戯倶楽部 (日本語: Japanese)</p>
-            <p>桌面游戏俱乐部 (中文: Chinese)</p>
-            <p>Club de jeux de table (Français: French)</p>
-            <p>Club de juegos de mesa (Español: Spanish)</p>
-            <p>Câu lạc bộ tabletop games (Tiếng Việt: Vietnamese)</p>
-          </div>
-        </div>
-      </section>
+      </div>
     );
   }
 }
---
+++ src/pages/tabletop-games.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import {Footer} from '../layout/Footer';
+import {Loader} from '../layout/Loader';
+import {JS} from '../layout/JS';
+import {Header} from '../layout/Header';
+import {Nav} from '../layout/Nav';
+import {Hero} from '../layout/Hero';
+import {Tege} from '../type/Tege';
+import {TegeIntroductions} from '../layout/TegeIntroductions';
+
+type Node = {
+  node: {
+    teges: Array<Tege>;
+  };
+};
+
+type Props = {
+  data: {
+    allConfigYaml: {
+      edges: Array<Node>;
+    };
+  };
+};
+type State = {
+};
+
+export class Events extends React.Component<Props, State> {
+
+  public render(): React.ReactElement {
+    const {
+      data: {
+        allConfigYaml: {
+          edges: [
+            {
+              node: {
+                teges
+              }
+            }
+          ]
+        }
+      }
+    } = this.props;
+
+    return (
+      <div>
+        <Header/>
+        <Nav/>
+        <Hero
+          jumbo='Tabletop games'
+          q='テーブルゲームの一覧を見る'
+        />
+        <TegeIntroductions
+          teges={teges}
+        />
+        <Footer/>
+        <Loader/>
+        <JS/>
+      </div>
+    );
+  }
+}
+
+export const pageQuery = graphql`
+query {
+  allConfigYaml {
+    edges {
+      node {
+        teges {
+          tegename
+          tegegenre
+          tegeeasiness
+          tegetime
+          tegepersons
+          tegeage
+          tegedescription
+          tegeimpression
+          tegeimage
+        }
+      }
+    }
+  }
+}
+`;
+
+export default Events;
