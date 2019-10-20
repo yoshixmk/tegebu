2334bee617b510c5ea2d7f85108eef68cc732e5c
jamashita <jamashita@arachnid.xyz>
2019-10-20 10:48:15 +0900

    description improved

--- src/layout/TegeSlogan.tsx
+++ src/layout/TegeSlogan.tsx
@@ -18,7 +18,7 @@ export class TegeSlogan extends React.Component<Props, State> {
           </span>
           <h2 className='mb-3'>私たちは日本で活動するテーブルゲームが大好きな団体です。これを「てげぶ」と呼んでいます。参加費は少額で、場合によって会場費が必要です。主催者のボランティアによって、活動は全て支えられています。</h2>
           <p>We are a tabletop games lover group in Japan. We call 'Tegebu' for ourselves. It is reasonable to participate. All activities are supported by the volunteers of the organizers.</p>
-          <p>Nous sommes un groupe d'amoureux de jeux de table au Japon. Nous appelons 'Tegebu' pour nous-mêmes. C'est bon marché pour participer. Toutes les acitivités sont soutenues par les bélévoles des organisateurs.</p>
+          <p>Nous sommes un groupe des amoureux de jeux de table au Japon. Nous appelons 'Tegebu' pour nous-mêmes. C'est bon marché pour participer. Toutes les acitivités sont soutenues par les bélévoles des organisateurs.</p>
           <p>Nosotros somos un grupo de los amantes de juegos de mesa en Japón. Nosotros llamamos 'Tegebu' para nosotros mismos. Es barato participar. Todas las activitidades son apoyadas por los volintarios de los organizores.</p>
         </div>
       </div>
